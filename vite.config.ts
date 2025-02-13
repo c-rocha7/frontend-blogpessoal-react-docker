da0302f6d88d5757f1dca2f03339d02e4928dfde
Cauã R. Pereira <7aauac@gmail.com>
2025-02-13 01:39:36 -0300

    :sparkles: feat: Adiciona dockerização do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Permite acesso externo
+    port: 5173,
+    watch: {
+      usePolling: true, // Necessário para hot reload no Docker
+    },
+    strictPort: true,
+    hmr: {
+      clientPort: 5173, // Garante que o HMR use a porta correta
+    },
+  },
+});
