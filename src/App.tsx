c61bf01859cbbe54fc5349ca00b99ae5f56e9158
Cauã R. Pereira <7aauac@gmail.com>
2025-02-14 12:16:34 -0300

    :sparkles: feat: Cria os componentes navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Footer from "./pages/components/footer/Footer";
+import Navbar from "./pages/components/navbar/Navbar";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
