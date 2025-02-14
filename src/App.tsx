d0d64cb88786afd1b2bf292e9020a1f58a7ce32a
Cauã R. Pereira <7aauac@gmail.com>
2025-02-14 14:11:25 -0300

    :bug: fix: Corrige a localização da pasta components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Footer from "./pages/components/footer/Footer";
-import Navbar from "./pages/components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 
 function App() {
