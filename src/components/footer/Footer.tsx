351bc909b063384dff79753af60212467618dcc0
Cauã R. Pereira <7aauac@gmail.com>
2025-02-14 15:27:59 -0300

    :sparkles: feat: Prepara as rotas do projeto

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,13 +12,19 @@ function Footer() {
       <div className="flex justify-center bg-indigo-900 text-white">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">
-            Blog Pessoal Generation | Copyright: {data}
+            Blog Pessoal Cauã R. Pereira | Copyright: {data}
           </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogo size={48} weight="bold" />
-            <InstagramLogo size={48} weight="bold" />
-            <FacebookLogo size={48} weight="bold" />
+            <a href="" target="_blank">
+              <LinkedinLogo size={48} weight="bold" />
+            </a>
+            <a href="" target="_blank">
+              <InstagramLogo size={48} weight="bold" />
+            </a>
+            <a href="" target="_blank">
+              <FacebookLogo size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
