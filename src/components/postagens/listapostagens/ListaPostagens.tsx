ba2839049b45fe562e41c5307e26df929a04adf1
Cauã R. Pereira <7aauac@gmail.com>
2025-02-20 15:30:00 -0300

    :sparkles: feat: Criada estrutura para o crud de Postagem

--- src/components/postagens/listapostagens/ListaPostagens.tsx
+++ src/components/postagens/listapostagens/ListaPostagens.tsx
@@ -1,23 +1,25 @@
-import { useContext, useEffect, useState } from "react";
-import { DNA } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
-import Tema from "../../../models/Tema";
-import CardTemas from "../cardtemas/CardTemas";
-import { buscar } from "../../../services/Service";
+import CardPostagens from "../cardpostagens/CardPostagens";
+import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
+import Postagem from "../../../models/Postagem";
+import { buscar } from "../../../services/Service";
+import { DNA } from "react-loader-spinner";
 
-function ListaTemas() {
+function ListaPostagens() {
   const navigate = useNavigate();
 
-  const [temas, setTemas] = useState<Tema[]>([]);
+  const [postagens, setPostagens] = useState<Postagem[]>([]);
 
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
 
-  async function buscarTemas() {
+  async function buscarPostagens() {
     try {
-      await buscar("/temas", setTemas, {
-        headers: { Authorization: token },
+      await buscar("/postagens", setPostagens, {
+        headers: {
+          Authorization: token,
+        },
       });
     } catch (error: any) {
       if (error.toString().includes("403")) {
@@ -28,18 +30,18 @@ function ListaTemas() {
 
   useEffect(() => {
     if (token === "") {
-      alert("Você precisa estar logado!");
+      alert("Você precisa estar logado");
       navigate("/");
     }
   }, [token]);
 
   useEffect(() => {
-    buscarTemas();
-  }, [temas.length]);
+    buscarPostagens();
+  }, [postagens.length]);
 
   return (
     <>
-      {temas.length === 0 && (
+      {postagens.length === 0 && (
         <DNA
           visible={true}
           height="200"
@@ -51,14 +53,14 @@ function ListaTemas() {
       )}
       <div className="flex justify-center w-full my-4">
         <div className="container flex flex-col mx-2">
-          {temas.length === 0 && (
+          {postagens.length === 0 && (
             <span className="text-3xl text-center my-8">
-              Nenhum Tema foi encontrado!
+              Nenhuma postagem foi encontrado!
             </span>
           )}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {temas.map((tema) => (
-              <CardTemas key={tema.id} tema={tema} />
+          <div className="container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {postagens.map((postagem) => (
+              <CardPostagens key={postagem.id} postagem={postagem} />
             ))}
           </div>
         </div>
@@ -67,4 +69,4 @@ function ListaTemas() {
   );
 }
 
-export default ListaTemas;
+export default ListaPostagens;
